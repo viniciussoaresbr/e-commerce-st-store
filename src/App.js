e016dba4ded7160a98d65353f61d1c10477b5fe5
Vinicius Soares <viniciussoaresmbr@gmail.com>
2021-09-29 23:03:08 -0300

    versão inicial do projeto

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import ProductListProvider from "./components/contexts/productsList";
+import ProductsField from "./components/productsField/ProductsField";
+import CartProvider from "./components/contexts/Cart";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ProductListProvider>
+        <CartProvider>
+          <ProductsField />
+        </CartProvider>
+      </ProductListProvider>
     </div>
   );
 }
