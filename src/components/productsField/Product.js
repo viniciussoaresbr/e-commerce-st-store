90d8c543f3dc595989c845b1635e2be8831e21e3
Vinicius Soares <viniciussoaresmbr@gmail.com>
2021-09-30 23:53:17 -0300

    adicionando o componente Product

--- src/components/productsField/Product.js
+++ src/components/productsField/Product.js
@@ -1,21 +1,29 @@
 import { useContext } from "react";
 import Button from "../common/button/Button";
 import { CartContext } from "../contexts/Cart";
+import "./Product.css";
 
 const Product = ({ product }) => {
   const { addProduct } = useContext(CartContext);
 
   return (
-    <div className="product" key={product.id}>
-      <h1 className="product__title">{product.name}</h1>
-      <h2 className="product__price">{product.price}</h2>
-      <p className="product__score">{product.score}</p>
-      <Button
-        text="Adicionar ao carrinho"
-        handleClick={() => {
-          addProduct(product);
-        }}
+    <div className="product">
+      <img
+        alt="cardGame"
+        className="product__image"
+        src={require(`../../assets/${product.image}`).default}
       />
+      <section className="product__info">
+        <h1 className="product__title">{product.name}</h1>
+        <h2 className="product__price">{`R$ ${product.price}`}</h2>
+        <p className="product__score">{`Pontuação: ${product.score}`}</p>
+        <Button
+          text="Adicionar ao carrinho"
+          handleClick={() => {
+            addProduct(product);
+          }}
+        />
+      </section>
     </div>
   );
 };
