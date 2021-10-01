d3a8ca44d3ceab21ab6b9412dee80d9408628b4a
Vinicius Soares <viniciussoaresmbr@gmail.com>
2021-09-30 23:55:48 -0300

    adicionando componentes ao App.js

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import "./App.css";
 import ProductListProvider from "./components/contexts/productsList";
 import ProductsField from "./components/productsField/ProductsField";
 import CartProvider from "./components/contexts/Cart";
+import Header from "./components/header/Header";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <ProductListProvider>
         <CartProvider>
           <ProductsField />
