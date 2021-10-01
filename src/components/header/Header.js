b98da98caf2cc117713df7dc4105976eb6208168
Vinicius Soares <viniciussoaresmbr@gmail.com>
2021-09-30 23:54:52 -0300

    adicionando componente header

---
+++ src/components/header/Header.js
@@ -0,0 +1,12 @@
+import "./Header.css";
+const Header = () => {
+  return (
+    <header className="header">
+      <h1 className="header__title">
+        SM <span className="header__title-red">Store</span>
+      </h1>
+    </header>
+  );
+};
+
+export default Header;
