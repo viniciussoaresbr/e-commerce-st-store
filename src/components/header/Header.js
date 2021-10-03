bb4686f0903579c0a609bae72bce30fd55a5cb9a
Vinicius Soares <viniciussoaresmbr@gmail.com>
2021-10-03 19:01:10 -0300

    adicionando ícone do carrinho de compras ao header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,11 +1,35 @@
+import { useContext, useState } from "react";
+import CartField from "../cart-field/CartField";
+import { CartContext } from "../contexts/Cart";
 import "./Header.css";
 const Header = () => {
+  const { cartProducts } = useContext(CartContext);
+  const [showCart, setShowCart] = useState(false);
   return (
-    <header className="header">
-      <h1 className="header__title">
-        SM <span className="header__title-red">Store</span>
-      </h1>
-    </header>
+    <>
+      <header
+        className="header"
+        id={cartProducts.length > 0 ? "headerPipe-green" : "headerPipe-red"}
+      >
+        <h1 className="header__title">
+          Games <span className="header__title-red">Store</span>
+        </h1>
+        <img
+          className="header__cart"
+          src={require("../../assets/cart-icon.svg").default}
+          onClick={() => {
+            setShowCart(true);
+          }}
+        ></img>
+      </header>
+      <CartField
+        classOverlay={showCart ? "overlay" : "overlay-none"}
+        classCart={showCart ? "cartField" : "cartField-hidden"}
+        closeCart={() => {
+          setShowCart(false);
+        }}
+      />
+    </>
   );
 };
 
