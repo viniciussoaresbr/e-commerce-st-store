25d3ce6ffe3dc4e32ad18167be3dffaf121a21ea
Vinicius Soares <viniciussoaresmbr@gmail.com>
2021-10-03 18:58:56 -0300

    incluindo header ao cart provider

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import "./App.css";
 import ProductListProvider from "./components/contexts/productsList";
-import ProductsField from "./components/productsField/ProductsField";
+import ProductsField from "./components/products-field/ProductsField";
 import CartProvider from "./components/contexts/Cart";
 import Header from "./components/header/Header";
 
 function App() {
   return (
     <div className="App">
-      <Header />
       <ProductListProvider>
         <CartProvider>
+          <Header />
           <ProductsField />
         </CartProvider>
       </ProductListProvider>
