9823a405504e9c42969025cfbfcd488d4cc19162
Vinicius Soares <viniciussoaresmbr@gmail.com>
2021-10-04 22:14:14 -0300

    ajustando o header e adicionando atributo alt a imagem

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -2,6 +2,7 @@ import { useContext, useState } from "react";
 import CartField from "../cart-field/CartField";
 import { CartContext } from "../contexts/Cart";
 import "./Header.css";
+
 const Header = () => {
   const { cartProducts } = useContext(CartContext);
   const [showCart, setShowCart] = useState(false);
@@ -12,9 +13,10 @@ const Header = () => {
         id={cartProducts.length > 0 ? "headerPipe-green" : "headerPipe-red"}
       >
         <h1 className="header__title">
-          Games <span className="header__title-red">Store</span>
+          St <span className="header__title-red">Store</span>
         </h1>
         <img
+          alt=""
           className="header__cart"
           src={require("../../assets/cart-icon.svg").default}
           onClick={() => {
