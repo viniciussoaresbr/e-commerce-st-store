88db2663d352ec93196535bcffecad3c45d82041
Vinicius Soares <viniciussoaresmbr@gmail.com>
2021-11-03 23:21:42 -0300

    utilizando o useReducer para o gerenciamento dos estados do context

---
+++ src/contexts/CartContext.jsx
@@ -0,0 +1,111 @@
+import { createContext, useEffect, useReducer } from "react";
+
+export const CartContext = createContext({});
+
+const CartProvider = ({ children }) => {
+  const cartReducer = (state, action) => {
+    switch (action.type) {
+      case "ADD_PRODUCT_TO_CART":
+        if (
+          state.cartProducts.some(product => {
+            return product.id === action.payload.product.id;
+          })
+        ) {
+          return {
+            ...state,
+            cartProducts: state.cartProducts.map(product => {
+              if (product.id === action.payload.product.id) {
+                return { ...product, quantity: product.quantity + 1 };
+              }
+              return product;
+            }),
+          };
+        } else {
+          return {
+            ...state,
+            cartProducts: [
+              ...state.cartProducts,
+              { ...action.payload.product, quantity: 1 },
+            ],
+          };
+        }
+      case "REMOVE_PRODUCT_FROM_CART":
+        return {
+          ...state,
+          cartProducts: state.cartProducts.filter(product => {
+            return product.id !== action.payload.id;
+          }),
+        };
+      case "CALC_SUBTOTAL":
+        return {
+          ...state,
+          subtotal: state.cartProducts.reduce((prevValue, product) => {
+            return prevValue + product.price * product.quantity;
+          }, 0),
+        };
+      case "CALC_SHIPPING":
+        const initialShipping = 10;
+        let totalAmount = state.cartProducts.reduce((prevValue, product) => {
+          return prevValue + product.quantity;
+        }, 0);
+        return {
+          ...state,
+          shipping: initialShipping * totalAmount,
+        };
+
+      case "CALC_TOTAL":
+        if (state.subtotal < 250) {
+          return {
+            ...state,
+            total: state.subtotal + state.shipping,
+          };
+        } else {
+          return {
+            ...state,
+            total: state.subtotal,
+          };
+        }
+    }
+  };
+
+  const [state, dispatch] = useReducer(cartReducer, {
+    cartProducts: [],
+    shipping: 0,
+    subtotal: 0,
+    total: 0,
+  });
+
+  useEffect(() => {
+    calcSubTotalPrice();
+    calcShipping();
+    calcTotalPrice();
+  }, [state.cartProducts]);
+
+  const addProductToCart = product => {
+    dispatch({ type: "ADD_PRODUCT_TO_CART", payload: { product } });
+  };
+
+  const removeProductFromCart = id => {
+    dispatch({ type: "REMOVE_PRODUCT_FROM_CART", payload: { id } });
+  };
+
+  const calcSubTotalPrice = () => dispatch({ type: "CALC_SUBTOTAL" });
+
+  const calcShipping = () => dispatch({ type: "CALC_SHIPPING" });
+
+  const calcTotalPrice = () => dispatch({ type: "CALC_TOTAL" });
+
+  return (
+    <CartContext.Provider
+      value={{
+        addProductToCart,
+        removeProductFromCart,
+        state,
+      }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
+};
+
+export default CartProvider;
