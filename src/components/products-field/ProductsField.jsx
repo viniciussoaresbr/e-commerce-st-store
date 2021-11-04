59ffce3ea4fa55c429e80dfa97221d87cd2c5a29
Vinicius Soares <viniciussoaresmbr@gmail.com>
2021-11-04 09:51:25 -0300

    adicionando ToastContainer

--- src/components/products-field/ProductsField.jsx
+++ src/components/products-field/ProductsField.jsx
@@ -2,16 +2,21 @@ import { useContext } from "react";
 import { ProductsListContext } from "../../contexts/ProductsListContext";
 import "./productsField.css";
 import Product from "./product/Product";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.min.css";
 
 const ProductsField = () => {
   const { products } = useContext(ProductsListContext);
 
   return (
-    <section className="productField">
-      {products.map(product => {
-        return <Product product={product} key={product.id} />;
-      })}
-    </section>
+    <>
+      <ToastContainer />
+      <section className="productField">
+        {products.map(product => {
+          return <Product product={product} key={product.id} />;
+        })}
+      </section>
+    </>
   );
 };
 
